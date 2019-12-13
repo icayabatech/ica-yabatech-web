9f81fa9487ad69df2ce9e249cfb9abb9245e8013
Destiny Ajakaiye <destinyajakaiye@gmail.com>
2019-12-13 17:08:51 +0100

    Added homepage | Added layout structure

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const HomePage = () => {
+    return (
+        <Fragment>
+            <div>Hello home page</div>
+        </Fragment>
+    );
+};
+
+export default HomePage;
