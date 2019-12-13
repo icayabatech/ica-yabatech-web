b87cf9f4f156f630e0757afa9ea45200cbd5697f
Destiny Ajakaiye <destinyajakaiye@gmail.com>
2019-12-13 17:23:42 +0100

    Added Top navigation | Add footer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,15 @@
 import React, { Fragment } from 'react';
+import TopNavigation from '../TopNavigation/TopNavigation';
+import FooterComponent from '../Footer/FooterComponent';
 
-const Layout = () => {
+const Layout = ({children}) => {
     return (
         <Fragment>
-
+            <TopNavigation></TopNavigation>
+            <main>
+                {children}
+            </main>
+            <FooterComponent></FooterComponent>
         </Fragment>
     );
 };
---
+++ src/components/TopNavigation/TopNavigation.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import './TopNavigation';
+
+const TopNavigation = () => {
+    return (
+        <Fragment>
+
+        </Fragment>
+    );
+};
+
+export default TopNavigation;
