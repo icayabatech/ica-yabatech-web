3a70fd1d0d3b84139ec40835ab85bd40216b8026
Destiny Ajakaiye <destinyajakaiye@gmail.com>
2019-12-14 16:46:31 +0100

    Added top section

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,11 +1,22 @@
 import React, { Fragment } from 'react';
 import Layout from '../../components/Layout/Layout';
+import './HomePage.css';
 
 const HomePage = () => {
     return (
         <Fragment>
             <Layout>
-                <div>Hello home page</div>
+                <section className="lead-board">
+                    <div className="container">
+                        <div className="col-lg-12 col-md-12 col-xs-12 col-sm-12">
+                            <span className="hoz-line"></span>
+                            <span className="welcome-text">Welcome to ICA</span>
+                            <h1 className="top-h1" style={{marginBottom: '0px'}}>Community</h1>
+                            <h1 className="top-h1-2" style={{marginTop: '0px'}}>For Creators</h1>
+                            <button type="button" className="top-btn btn btn-lg">Become a member</button>
+                        </div>
+                    </div>
+                </section>
             </Layout>
         </Fragment>
     );
